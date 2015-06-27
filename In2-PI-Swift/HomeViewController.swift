25e8e3ee3e84eb0eb6dbb1095cbfc311a7283355
Terry Bu <burea1124@gmail.com>
2015-06-27 15:59:35 -0400

    Setting up localizations for korean

--- In2-PI-Swift/HomeViewController.swift
+++ In2-PI-Swift/HomeViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let name = String(format: NSLocalizedString("name", comment: ""))
+        //this comment param for NSLocalizedString is really annoying and unnecessary because you can't set it to nil
+        let msg = String(format: NSLocalizedString("Hello %@", comment: ""), name)
+     
+        println(msg)
     }
 
     override func didReceiveMemoryWarning() {
