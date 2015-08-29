a701009728823f4fde50a31d1a1a3f47f7d87390
Terry Bu <burea1124@gmail.com>
2015-08-29 19:34:53 -0400

    Readding back facebook grab public photos album code to Gallery

--- In2-PI-Swift/GalleryViewController.swift
+++ In2-PI-Swift/GalleryViewController.swift
@@ -7,13 +7,64 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import SwiftyJSON
+import AFNetworking
 
 class GalleryViewController: ParentViewController {
     
+    var photosArray : [JSON]!
+    var initialY : CGFloat!
     
     override func viewDidLoad() {
         setUpStandardUIForViewControllers()
         
+        var paramsDictionary = [
+            "access_token": kAppAccessToken
+        ]
+        FBSDKGraphRequest(graphPath: "1384548091800506/albums", parameters: paramsDictionary).startWithCompletionHandler { (connection, data, error) -> Void in
+            
+            //Terry's test page
+            //752896354818721/albums will get you the page's albums
+            //757485894359767 is Untitled Album
+            //757485937693096 is one image
+            
+            //In2 real page PI magazine
+            //1384548091800506/albums will get you page's albums
+            
+            if (error == nil) {
+                let jsonData = JSON(data)
+                let albumsList = jsonData["data"]
+                let firstAlbumID = albumsList[0]["id"].string
+                let lastParamsDictionary = [
+                    "access_token": kAppAccessToken,
+                    "fields" : "photos{picture}"
+                    
+                ]
+                FBSDKGraphRequest(graphPath: firstAlbumID, parameters: lastParamsDictionary).startWithCompletionHandler { (connection, albumPhotos, error) -> Void in
+                    println(albumPhotos)
+                    let albumPhotosJSON = JSON(albumPhotos)
+                    let photos = albumPhotosJSON["photos"]
+                    self.photosArray = photos["data"].arrayValue
+                    self.initialY = 50.0
+                    for photoObject in self.photosArray {
+                        let photoURLString = photoObject["picture"].string
+                        dispatch_async(dispatch_get_main_queue(),{
+                            if let initial = self.initialY {
+                                var imageView = UIImageView(frame: CGRectMake(50.0,self.initialY,200.0,200.0))
+                                imageView.setImageWithURL(NSURL(string: photoURLString!))
+                                self.view.addSubview(imageView)
+                                self.initialY = self.initialY + 200
+                            }
+                        })
+                    }
+                }
+            }
+            else {
+                println(error)
+            }
+        }
+
     }
     
 }
