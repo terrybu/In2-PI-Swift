a419009b8c9ea50a8e93855e773886d18e3b483f
Terry Bu <burea1124@gmail.com>
2015-08-30 11:40:26 -0400

    Refactored facebook logic code out of VC, started implementing gallery collection view

---
+++ In2-PI-Swift/FacebookManager.swift
@@ -0,0 +1,61 @@
+//
+//  FacebookManager.swift
+//  In2-PI-Swift
+//
+//  Created by Terry Bu on 8/30/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+import Foundation
+import FBSDKLoginKit
+import SwiftyJSON
+
+protocol FacebookManagerDelegate {
+    func didFinishGettingFacebookPhotos(jsonArray: [JSON])
+}
+
+class FacebookManager {
+    
+    static let sharedInstance = FacebookManager()
+    var delegate: FacebookManagerDelegate?
+    
+    func getPhotosFromFacebookAlbum() {
+        var paramsDictionary = [
+            "access_token": kAppAccessToken
+        ]
+        FBSDKGraphRequest(graphPath: "1384548091800506/albums", parameters: paramsDictionary).startWithCompletionHandler { (connection, data, error) -> Void in
+            
+            //Terry's test page
+            //752896354818721/albums will get you the page's albums
+            //757485894359767 is Untitled Album
+            //757485937693096 is one image
+            
+            //In2 real page PI magazine
+            //1384548091800506/albums will get you page's albums
+            
+            if (error == nil) {
+                let jsonData = JSON(data)
+                let albumsList = jsonData["data"]
+                let firstAlbumID = albumsList[0]["id"].string
+                let lastParamsDictionary = [
+                    "access_token": kAppAccessToken,
+                    "fields" : "photos{picture}"
+                    
+                ]
+                FBSDKGraphRequest(graphPath: firstAlbumID, parameters: lastParamsDictionary).startWithCompletionHandler { (connection, albumPhotos, error) -> Void in
+//                    println(albumPhotos)
+                    let albumPhotosJSON = JSON(albumPhotos)
+                    let photos = albumPhotosJSON["photos"]
+                    let photosArray = photos["data"].arrayValue
+                    if let delegate = self.delegate {
+                        delegate.didFinishGettingFacebookPhotos(photosArray)
+                    }
+                }
+            }
+            else {
+                println(error)
+            }
+        }
+    }
+    
+}
--- In2-PI-Swift/HideKey.swift
+++ In2-PI-Swift/HideKey.swift
@@ -7,3 +7,5 @@
 //
 
 let kAppAccessToken = "424019454456359|1bKu9Z_qb5Ppyw5ysZ1-MSeh-So"
+//this is the token for the entire app (In2 iOS) on Facebook Dashboard that I registered
+//using this token can allow you to easily get public open info like photos from a page
