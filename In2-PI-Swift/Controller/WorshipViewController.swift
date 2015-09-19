061c606b8b89d011a1cefda6d45673f1cac6d3d8
Terry Bu <burea1124@gmail.com>
2015-09-19 16:21:01 -0400

    Moving away from AFNetworking to just using NSURLSession

--- In2-PI-Swift/Controller/WorshipViewController.swift
+++ In2-PI-Swift/Controller/WorshipViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
-import WebKit
 import SafariServices
+import AFNetworking
+import SwiftyJSON
 
 private let kOriginalAboutViewHeight: CGFloat = 32.0
 private let kExpandedAboutViewHeight: CGFloat = 300.0
@@ -35,8 +36,22 @@ class WorshipViewController: ParentViewController, SFSafariViewControllerDelegat
         songsArray = ["Hillsong - Above All", "예수전도단 - 좋으신 하나님", "예수전도단 - 주 나의 왕"]
         let test1 = "07/19/2015"
         let test2 = "07/12/2015"
+        
+        getDataFromImportIOAPIForJoobos()
         joobosArray = [test1, test2]
         jooboTableView.reloadData()
+    }
+    
+    private func getDataFromImportIOAPIForJoobos() {
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET("https://api.import.io/store/data/3bc15cdb-dbc3-4e1a-b937-df3c2a68dbbc/_query?input/webpage/url=http%3A%2F%2Fvision.onnuri.org%2Fin2%2Farchives%2Fsunday_bulletin_category%2Fsunday-bulletin&_user=0a668a36-6aa0-4bf7-a33f-4aa867422551&_apikey=0a668a366aa04bf7a33f4aa86742255106cd4c765c1dc526570f30d171b722431f993ad7303c4f09ce394851f9fc53aecb7bf49dd2f2f02cd8e0624fa8dedf188642ab734d5c9159f291501b1b381633", parameters: nil,
+            success: { (operation, responseObject) -> Void in
+//                print(responseObject)
+                print(JSON(responseObject))
+            },
+            failure: { (operation, error) -> Void in
+                print(error)
+            })
     }
     
     private func setUpExpandableAboutView() {
