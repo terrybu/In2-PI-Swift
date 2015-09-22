e2b815dcdb20a9ec8d2a8c2cb4bf7328867d10be
Terry Bu <burea1124@gmail.com>
2015-09-22 12:21:58 -0400

    Added UIActivityIndicator view for loading weekly programs off import io

--- In2-PI-Swift/Controller/WorshipViewController.swift
+++ In2-PI-Swift/Controller/WorshipViewController.swift
@@ -8,21 +8,20 @@
 
 import UIKit
 import SafariServices
-import AFNetworking
-import SwiftyJSON
 
 private let kOriginalAboutViewHeight: CGFloat = 32.0
 private let kExpandedAboutViewHeight: CGFloat = 300.0
 private let kOriginalContentViewHeight: CGFloat = 600
 
-class WorshipViewController: ParentViewController, SFSafariViewControllerDelegate, UITableViewDelegate, UITableViewDataSource {
+class WorshipViewController: ParentViewController, WeeklyProgramDownloaderDelegate ,SFSafariViewControllerDelegate, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet var contentView: UIView! //this property might not actually be needed ..
+    @IBOutlet var contentView: UIView!
     @IBOutlet var expandableAboutView: ExpandableAboutView!
     @IBOutlet var songsTableView :     UITableView!
     @IBOutlet var weeklyProgramsTableView :     UITableView!
     var songsArray = [String]()
     var weeklyProgramsArray = [WeeklyProgram]()
+    let indicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
     
     //Constraints
     @IBOutlet weak var constraintHeightExpandableView: NSLayoutConstraint!
@@ -35,36 +34,14 @@ class WorshipViewController: ParentViewController, SFSafariViewControllerDelegat
         
         songsArray = ["Hillsong - Above All", "예수전도단 - 좋으신 하나님", "예수전도단 - 주 나의 왕"]
         
-        getDataFromImportIOAPIForWeeklyPrograms()
-        weeklyProgramsTableView.reloadData()
-    }
-    
-    private func getDataFromImportIOAPIForWeeklyPrograms() {
-        let manager = AFHTTPSessionManager()
-        manager.requestSerializer = AFJSONRequestSerializer()
-        manager.responseSerializer = AFJSONResponseSerializer()
-        manager.GET("https://api.import.io/store/data/3bc15cdb-dbc3-4e1a-b937-df3c2a68dbbc/_query?input/webpage/url=http%3A%2F%2Fvision.onnuri.org%2Fin2%2Farchives%2Fsunday_bulletin_category%2Fsunday-bulletin&_user=0a668a36-6aa0-4bf7-a33f-4aa867422551&_apikey=0a668a366aa04bf7a33f4aa86742255106cd4c765c1dc526570f30d171b722431f993ad7303c4f09ce394851f9fc53aecb7bf49dd2f2f02cd8e0624fa8dedf188642ab734d5c9159f291501b1b381633", parameters: nil,
-            success: { (task:NSURLSessionDataTask!, responseObject:AnyObject!) -> Void in
-                let jsonData = JSON(responseObject)
-                let resultsArray = jsonData["results"].arrayValue
-                print(resultsArray)
-                for dict:JSON in resultsArray {
-                    let title = dict["tit_link/_text"].stringValue
-                    let datesArray = dict["tit_link_numbers"].arrayValue
-                    let dateString = "\(datesArray[0].stringValue) \(datesArray[1].stringValue) \(datesArray[2].stringValue)"
-                    let url = dict["tit_link"].stringValue
-                    let newWeeklyProgram = WeeklyProgram(title: title, url: url, dateString: dateString)
-                    self.weeklyProgramsArray.append(newWeeklyProgram)
-                }
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
-                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        self.weeklyProgramsTableView.reloadData()
-                    })
-                })
-            },
-            failure: { (task:NSURLSessionDataTask!, error:NSError!) -> Void in
-                print(error)
-            })
+        if weeklyProgramsArray.isEmpty {
+            indicator.center = CGPointMake(view.center.x, weeklyProgramsTableView.center.y)
+            view.addSubview(indicator)
+            indicator.startAnimating()
+        }
+        
+        WeeklyProgramDownloader.sharedInstance.delegate = self
+        WeeklyProgramDownloader.sharedInstance.getTenRecentWeeklyProgramsListFromImportIO()
     }
     
     private func setUpExpandableAboutView() {
@@ -77,6 +54,16 @@ class WorshipViewController: ParentViewController, SFSafariViewControllerDelegat
         print("worship vc viewdidappear")
     }
     
+    
+    //MARK: WeeklyProgramDownloader Delegate methods
+    func didFinishDownloadinglistOfTenWeeklyProgramsFromImportIO(downloadedProgramsArray: [WeeklyProgram]?) {
+        if let downloadedProgramsArray = downloadedProgramsArray {
+            self.weeklyProgramsArray = downloadedProgramsArray
+            weeklyProgramsTableView.reloadData()
+            indicator.stopAnimating()
+            weeklyProgramsTableView.hidden = false
+        }
+    }
     
     //MARK: WeeklyPrograms TableView Delegate methods
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -137,15 +124,16 @@ class WorshipViewController: ParentViewController, SFSafariViewControllerDelegat
         if (tableView == weeklyProgramsTableView) {
             let reuseIdentifier = "WeeklyProgramsTableViewCell"
             cell = weeklyProgramsTableView.dequeueReusableCellWithIdentifier(reuseIdentifier)!
-            cell.textLabel!.text = weeklyProgramsArray[indexPath.row].title
-            cell.accessoryView = UIImageView(image: UIImage(named: "btn_download"))
-            return cell
+
+                cell.textLabel!.text = weeklyProgramsArray[indexPath.row].title
+                cell.accessoryView = UIImageView(image: UIImage(named: "btn_download"))
+
         } else {
             let reuseIdentifier = "SongsTableViewCell"
             cell = songsTableView.dequeueReusableCellWithIdentifier(reuseIdentifier)!
             cell.textLabel!.text = songsArray[indexPath.row]
-            return cell
         }
+        return cell
     }
     
     //MARK: UITableViewDelegate
--- In2-PI-Swift/Model/WeeklyProgramDownloader.swift
+++ In2-PI-Swift/Model/WeeklyProgramDownloader.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2015 Terry Bu. All rights reserved.
 //
 
+import AFNetworking
+import SwiftyJSON
+
 private let kImportIOURLForScrapingTenRecentWeeklyPrograms =  "https://api.import.io/store/data/3bc15cdb-dbc3-4e1a-b937-df3c2a68dbbc/_query?input/webpage/url=http%3A%2F%2Fvision.onnuri.org%2Fin2%2Farchives%2Fsunday_bulletin_category%2Fsunday-bulletin&_user=0a668a36-6aa0-4bf7-a33f-4aa867422551&_apikey=0a668a366aa04bf7a33f4aa86742255106cd4c765c1dc526570f30d171b722431f993ad7303c4f09ce394851f9fc53aecb7bf49dd2f2f02cd8e0624fa8dedf188642ab734d5c9159f291501b1b381633"
 
 protocol WeeklyProgramDownloaderDelegate {
-    func listOfTenWeeklyProgramsDownloadedFromImportIO()
+    func didFinishDownloadinglistOfTenWeeklyProgramsFromImportIO(downloadedProgramsArray: [WeeklyProgram]?)
     
 }
 
@@ -17,8 +20,30 @@ class WeeklyProgramDownloader {
     
     static let sharedInstance = WeeklyProgramDownloader()
     var delegate: WeeklyProgramDownloaderDelegate?
-    var weeklyProgramsListArray = [WeeklyProgram]()
+    var weeklyProgramsArray = [WeeklyProgram]()
     
-    
+    func getTenRecentWeeklyProgramsListFromImportIO() {
+        let manager = AFHTTPSessionManager()
+        manager.requestSerializer = AFJSONRequestSerializer()
+        manager.responseSerializer = AFJSONResponseSerializer()
+        manager.GET(kImportIOURLForScrapingTenRecentWeeklyPrograms, parameters: nil,
+            success: { (task:NSURLSessionDataTask!, responseObject:AnyObject!) -> Void in
+                let jsonData = JSON(responseObject)
+                let resultsArray = jsonData["results"].arrayValue
+                print(resultsArray)
+                for dict:JSON in resultsArray {
+                    let title = dict["tit_link/_text"].stringValue
+                    let datesArray = dict["tit_link_numbers"].arrayValue
+                    let dateString = "\(datesArray[0].stringValue) \(datesArray[1].stringValue) \(datesArray[2].stringValue)"
+                    let url = dict["tit_link"].stringValue
+                    let newWeeklyProgram = WeeklyProgram(title: title, url: url, dateString: dateString)
+                    self.weeklyProgramsArray.append(newWeeklyProgram)
+                }
+                self.delegate?.didFinishDownloadinglistOfTenWeeklyProgramsFromImportIO(self.weeklyProgramsArray)
+            },
+            failure: { (task:NSURLSessionDataTask!, error:NSError!) -> Void in
+                print(error)
+        })
+    }
     
 }
