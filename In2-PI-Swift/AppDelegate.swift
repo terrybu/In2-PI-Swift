380dc589cb490fb79948f8294647026b5ad65c0d
Terry Bu <burea1124@gmail.com>
2015-11-15 17:04:12 -0500

    Putting together UI for signup screen

--- In2-PI-Swift/AppDelegate.swift
+++ In2-PI-Swift/AppDelegate.swift
@@ -58,7 +58,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     @objc
     private func moviePlayBackDidFinish() {
-        #if RELEASE
+//        #if RELEASE
+//            let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
+//            loginVC.dismissBlock = {
+//                let revealVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("SWRevealViewController") as! SWRevealViewController
+//                self.window?.rootViewController = revealVC
+//                print("dismiss block executing from appdelegate")
+//                self.setUpNavBarAndStatusBarImages()
+//            }
+//            window?.rootViewController = loginVC
+//        #else
+//            //NO LOGIN FOR DEBUG JUST FOR NOW - comment it out to test Login screen
+//            let revealVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("SWRevealViewController") as! SWRevealViewController
+//            self.window?.rootViewController = revealVC
+//        #endif
             let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("LoginViewController") as! LoginViewController
             loginVC.dismissBlock = {
                 let revealVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("SWRevealViewController") as! SWRevealViewController
@@ -67,11 +80,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 self.setUpNavBarAndStatusBarImages()
             }
             window?.rootViewController = loginVC
-        #else
-            //NO LOGIN FOR DEBUG JUST FOR NOW - comment it out to test Login screen
-            let revealVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("SWRevealViewController") as! SWRevealViewController
-            self.window?.rootViewController = revealVC
-        #endif
+
+
         
         setUpNavBarAndStatusBarImages()
     }
