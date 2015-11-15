23f8fcabdedf9a2ae0ee1394ff5c496323c55308
Terry Bu <burea1124@gmail.com>
2015-11-15 18:40:21 -0500

    Basic parse code for signup complete

--- In2-PI-Swift/Controller/SignUpViewController.swift
+++ In2-PI-Swift/Controller/SignUpViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2015 Terry Bu. All rights reserved.
 //
 
+import Parse
 import UIKit
 
 class SignUpViewController: UIViewController {
@@ -14,13 +15,71 @@ class SignUpViewController: UIViewController {
     @IBOutlet var lastNameTextField:PaddedTextField!
     @IBOutlet var userNameTextField:PaddedTextField!
     @IBOutlet var passwordTextField:PaddedTextField!
+    @IBOutlet var emailTextField:PaddedTextField!
 
     
     @IBAction func backArrowButton(sender: AnyObject) {
-        
         navigationController?.popViewControllerAnimated(true)
-        
     }
+    
+    @IBAction func signUpButtonPressed(sender: AnyObject) {
+        var firstName = self.firstNameTextField.text
+        var lastName = self.lastNameTextField.text
+        var username = self.userNameTextField.text
+        var password = self.passwordTextField.text
+        var email = self.emailTextField.text
+        // Validate the text fields
+//        if username?.characters.count < 5 {
+//            var alert = UIAlertView(title: "Invalid", message: "Username must be greater than 5 characters", delegate: self, cancelButtonTitle: "OK")
+//            alert.show()
+//            
+//        } else 
+//        if password?.characters.count < 8 {
+//            var alert = UIAlertView(title: "Invalid", message: "Password must be greater than 8 characters", delegate: self, cancelButtonTitle: "OK")
+//            alert.show()
+        
+//        } else 
+        
+        if email?.characters.count < 8 {
+            var alert = UIAlertView(title: "Invalid", message: "Please enter a valid email address", delegate: self, cancelButtonTitle: "OK")
+            alert.show()
+            
+        } else {
+            // Run a spinner to show a task in progress
+            var spinner: UIActivityIndicatorView = UIActivityIndicatorView(frame: CGRectMake(0, 0, 150, 150)) as UIActivityIndicatorView
+            spinner.startAnimating()
+            
+            var newUser = PFUser()
+            newUser.username = username
+            newUser.password = password
+            newUser.setValue(firstName, forKey: "firstName")
+            newUser.setValue(lastName, forKey: "lastName")
+            newUser.setValue(email, forKey: "email")
+            
+            // Sign up the user asynchronously
+            newUser.signUpInBackgroundWithBlock({ (succeed, error) -> Void in
+                
+                // Stop the spinner
+                spinner.stopAnimating()
+                if ((error) != nil) {
+                    //error case
+                    var alert = UIAlertView(title: "Error", message: "\(error)", delegate: self, cancelButtonTitle: "OK")
+                    alert.show()
+                } else {
+                    //success case
+                    var alert = UIAlertView(title: "Success", message: "Signed Up", delegate: self, cancelButtonTitle: "OK")
+                    alert.show()
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        //do some success navigation
+                        print("sign up success!!")
+                        
+                    })
+                }
+            })
+        }
+
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
