552999a13001509f0fe407ea6bf521393f31c21b
Terry Bu <burea1124@gmail.com>
2016-02-07 11:27:33 -0500

    Fix social service screen without calendar

--- In2-PI-Swift/Controller/SocialServicesViewController.swift
+++ In2-PI-Swift/Controller/SocialServicesViewController.swift
@@ -16,7 +16,11 @@ class SocialServicesViewController: ParentViewController, UITableViewDelegate, U
     @IBOutlet var expandableAboutView: ExpandableAboutView!
     @IBOutlet weak var constraintHeightExpandableView: NSLayoutConstraint!
     @IBOutlet weak var constraintContentViewHeight: NSLayoutConstraint!
+
     @IBOutlet var tableView: UITableView!
+    var eventsArray: [SocialServiceEvent]?
+    
+    
     @IBAction func applyButtonPressed() {
         self.presentSFSafariVCIfAvailable(NSURL(string: kApplySocialServicesTeamGoogleDocURL)!)
         
@@ -27,6 +31,22 @@ class SocialServicesViewController: ParentViewController, UITableViewDelegate, U
         setUpExpandableAboutView(kOriginalAboutViewHeight, expandableAboutView: expandableAboutView, heightBuffer: 30, view: view, constraintHeightExpandableView: constraintHeightExpandableView, constraintContentViewHeight: constraintContentViewHeight, originalContentviewHeight: kOriginalContentViewHeight)
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(false)
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+        activityIndicator.center = CGPointMake(view.center.x, 100)
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        FirebaseManager.sharedManager.getServiceEventObjectsFromFirebase({
+            (success) -> Void in
+            if (success) {
+                self.eventsArray = FirebaseManager.sharedManager.eventsArray
+                self.tableView.reloadData()
+            }
+            activityIndicator.stopAnimating()
+        })
+    }
+    
     
     //MARK: UITableViewDataSource
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -34,18 +54,33 @@ class SocialServicesViewController: ParentViewController, UITableViewDelegate, U
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        if let eventsArray = eventsArray {
+            return eventsArray.count
+        }
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "SocialServicesCell")
-        configureCell(cell, forRowAtIndexPath: indexPath)
+        let cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "reuse")
+        
+        let event = eventsArray![indexPath.row]
+        cell.textLabel!.text = event.title
+        cell.detailTextLabel!.text = event.date
+
         return cell
     }
     
-    func configureCell(cell: UITableViewCell, forRowAtIndexPath: NSIndexPath) {
-        cell.textLabel!.text = "긍휼부 이벤트 0\(forRowAtIndexPath.row)"
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 40))
+        let label = UILabel(frame: CGRectMake(12, 5, 300, 18))
+        label.text = "긍휼부 이벤트 리스트"
+        headerView.backgroundColor = UIColor.whiteColor()
+        label.textColor = UIColor.blackColor()
+        label.font = UIFont.boldSystemFontOfSize(17)
+        headerView.addSubview(label)
+        return headerView
     }
+
     
     //Allowing storyboard to load this VC from XIB
     required init(coder aDecoder: NSCoder) {
