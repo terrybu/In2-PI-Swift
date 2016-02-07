2f16ef0fbfe3b2086377ed9f37bf31e20cc023f0
Terry Bu <burea1124@gmail.com>
2016-02-07 07:44:33 -0500

    Started NoticeCreation page for Admin

---
+++ In2-PI-Swift/Controller/NoticeCreationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NoticeCreationViewController.swift
+//  In2-PI-Swift
+//
+//  Created by Terry Bu on 2/7/16.
+//  Copyright © 2016 Terry Bu. All rights reserved.
+//
+
+import UIKit
+
+class NoticeCreationViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: PaddedTextField!
+    @IBOutlet weak var bodyTextField: PaddedTextField!
+    @IBOutlet weak var linkTextField: PaddedTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
